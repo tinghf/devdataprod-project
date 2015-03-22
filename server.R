e3f391a1615445ab540c230a9b60c88eaadecde3
Ting <tinghf@gmail.com>
2015-03-22 15:57:19 -0700

    added shiny app content
    Signed-off-by: Ting <tinghf@gmail.com>

--- server.R
+++ server.R
@@ -1,10 +1,36 @@
+# http://cran.r-project.org/web/packages/rpart.plot/rpart.plot.pdf
+# http://www.milbo.org/rpart-plot/prp.pdf
 library(shiny)
+library(rpart)
+library(rpart.plot)
+data(ptitanic) 
+tree <- rpart(survived ~ ., data=ptitanic, cp=.02) # cp=.02 because want small tree for demo
 
 
-diabetesRisk <- function(glucose) glucose / 200
-shinyServer(
-  function(input, output) {
-    output$inputValue <- renderPrint({input$glucose})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-  }
-)
+shinyServer(function(input, output, session) {
+
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    
+    cols <- ifelse(tree$frame$yval == 1, input$survive.col, input$died.col)
+    # green if survived
+    prp(tree, main="assorted arguments",
+    extra=106, # display prob of survival and percent of obs
+    nn=input$nn, # display the node numbers
+    fallen.leaves=input$fallen.leaves, # put the leaves on the bottom of the page
+    branch=.5, # change angle of branch lines
+    faclen=0, # do not abbreviate factor levels
+    trace=input$trace, # print the automatically calculated cex
+    shadow.col=input$shadow.col, # shadows under the leaves
+    branch.lty=3, # draw branches using dotted lines
+    split.cex=1.2, # make the split text larger than the node text
+    split.prefix=input$split.prefix,  # put "is " before split text
+    split.suffix=input$split.suffix, # put "?" after split text
+    col=cols, border.col=cols, # green if survived
+    split.box.col="lightgray", # lightgray split boxes (default is white)
+    split.border.col="darkgray", # darkgray border on split boxes
+    split.round=input$split.round) # round the split box corners a tad
+
+  })
+
+})
--- ui.R
+++ ui.R
@@ -1,18 +1,21 @@
+shinyUI(pageWithSidebar(
+  headerPanel("RPart prp plot"),
+  sidebarPanel(
+    checkboxInput("nn", p("Display the node numbers?", value = TRUE)),
+    checkboxInput("fallen.leaves", p("Put the leaves on the bottom of the page?", value = TRUE)),
+    checkboxInput("trace", p("Print the automatically calculated cex?", value = TRUE)),
+    selectInput('shadow.col', 'Color of the shadow', c("", "gray", "darkgray"), selected="gray"),
+    # selectInput('branch.lty', 'Line type ', c(1,2,3,4,5), selected = 1),
+    textInput('split.prefix', 'Text before Split=', value="is "),
+    textInput('split.suffix', 'Text after Split=', value="?"),
+    selectInput('survive.col', 'Survive col', colors(), selected="green4"),
+    selectInput('died.col', 'Died col', colors(), selected="darkred"),
+    numericInput('split.round', 'Controls the rounding of the corners of the split boxes', value=0)
+                  
+  ),
 
-shinyUI(
-  pageWithSidebar(
-    # Application title
-    headerPanel("Diabetes prediction"),
-    sidebarPanel(
-      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
-      submitButton('Submit')
-    ),
-    mainPanel(
-      h3('Results of prediction'),
-      h4('You entered'),
-      verbatimTextOutput("inputValue"),
-      h4('Which resulted in a prediction of '),
-      verbatimTextOutput("prediction")
-    )
-  )
-)
+  mainPanel(
+    h3('rpart model plot output'),
+    plotOutput('plot1')
+  )  
+))
